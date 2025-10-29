b1219dc34eb861c8f424042d6cd42b78a1b6a6ca
PPEACH21 <sohizz1201@gmail.com>
2025-10-29 18:57:36 +0700

    inti:  init Project

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+import {m} from './src/paraglide/messages.js'
+import { getLocale, setLocale } from './src/paraglide/runtime.js';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [language,setLaguage] = useState(getLocale());
+  const toggleLanguage = () => {
+    const newLang = language === "th" ? "en" : "th";
+    setLocale(newLang);
+    setLaguage(newLang);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{m.login()}</Text>
+      <Button title="CHANGE LANGUAGE" onPress={toggleLanguage} />
       <StatusBar style="auto" />
     </View>
   );
