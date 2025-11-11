8b233c63adf198e97ccafe16444a887f75116eba
PPEACH21 <sohizz1201@gmail.com>
2025-11-11 23:54:31 +0700

    feat(frontend):boutton nav for vendor

--- Client/src/Vendor/V_ButtonNav.js
+++ Client/src/Vendor/V_ButtonNav.js
@@ -10,37 +10,145 @@ import OrderShopScreen from "./pages/OrderShop";
 import ReserveShopScreen from "./pages/ResevationShop";
 import SettingShopScreen from "./pages/SettingShop";
 import { BaseColor as c } from "../components/Color";
+import { Text,View ,TouchableOpacity,Dimensions } from "react-native";
+import { Layout } from "../components/Layout";
+import { useEffect } from "react";
 
+import Animated, { useSharedValue, withTiming, useAnimatedStyle, Easing ,withSequence, useDerivedValue,} from 'react-native-reanimated';
 const Tab = createBottomTabNavigator();
+
+const TabStyle =({ state, descriptors, navigation }) => {
+
+
+  const translateY = useSharedValue(300); 
+  React.useEffect(() => {
+    translateY.value = withTiming(0, { duration: 2000, easing: Easing.out(Easing.exp) });
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+  }));
+  
+  const { width } = Dimensions.get("window");
+  const TAB_COUNT = state.routes.length;
+  const TAB_WIDTH = (width / TAB_COUNT)*0.83; // ระยะห่างแต่ละปุ่ม
+  const indicatorX = useSharedValue(0); // เก็บตำแหน่งวงกลม
+  const indicatorScale = useSharedValue(1);
+  useEffect(() => {
+    indicatorX.value = withTiming(state.index * TAB_WIDTH, { duration: 300 });
+    indicatorScale.value = withSequence(
+      withTiming(0.5, { duration: 200,easing: Easing.inOut(Easing.quad)}), 
+      withTiming(1, { duration: 200,easing: Easing.inOut(Easing.quad)}) 
+    );
+  }, [state.index]);
+
+  const indicatorStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: indicatorX.value-3},
+      { scale: indicatorScale.value },
+    ],
+  }));
+
+  
+  return (
+    <Animated.View style={[Layout.rowset ,{
+      position:'absolute',
+      alignSelf:'center',
+      backgroundColor:c.white, 
+      padding:10,
+      paddingVertical:20,
+      borderRadius:40,
+      bottom:15,
+      shadowColor:c.black,
+      gap:5
+    },animatedStyle
+    ]}>
+
+      <Animated.View style={[{position:'absolute',left:0,width:80,height:75,borderRadius:60,backgroundColor:c.S1},indicatorStyle]}/>
+
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const scale = useSharedValue(isFocused ? 1.3 : 1.0);
+        useDerivedValue(() => {
+          scale.value = withTiming(isFocused ? 1.3 : 1.0, {
+            duration: 250,
+          });
+        }, [isFocused]);
+
+        const animatedIconStyle = useAnimatedStyle(() => ({
+          transform: [{ scale: scale.value }],
+        }));
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+
+          scale.value = withTiming(1.5, { duration: 100 }, () => {
+            scale.value = withTiming(1.0, { duration: 200 });
+          });
+        
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        let iconName = "home-outline";
+        if (route.name === "Home") iconName = isFocused ? "home-outline" : "home-outline";
+        else if (route.name === "Orders") iconName = isFocused ? "receipt-outline" : "receipt-outline";
+        else if (route.name === "Menu") iconName = isFocused ? "restaurant-outline" : "restaurant-outline";
+        else if (route.name === "Reserve") iconName = isFocused ? "calendar-outline" : "calendar-outline";
+        else if (route.name === "Settings") iconName = isFocused ? "settings-outline" : "settings-outline";
+
+        
+
+        return (
+          <TouchableOpacity
+            key={route.key}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{justifyContent:'center',alignItems:'center'}}
+          >
+            <Animated.View style={[animatedIconStyle,{alignItems:'center', width:55}]}>
+              <Ionicons name={iconName} size={24} color={isFocused ? c.white : c.black} />
+              <Text style={{ color: isFocused ? c.white : c.black , fontWeight: "700", fontSize: 10 }}>
+                {label}
+              </Text>
+            </Animated.View>
+          </TouchableOpacity>
+        );
+      })}
+    </Animated.View>
+  );
+};
+
 
 export default function V_ButtonNav({ shopId, initialRouteName = "Home" }) {
   return (
     <Tab.Navigator
       initialRouteName={String(initialRouteName || "Home")}
-      screenOptions={({ route }) => {
-        const icon = (focused, size, color) => {
-          let name = "home-outline";
-          if (route.name === "Home") name = focused ? "home" : "home-outline";
-          else if (route.name === "Orders") name = focused ? "receipt" : "receipt-outline";
-          else if (route.name === "Menu") name = focused ? "restaurant" : "restaurant-outline";
-          else if (route.name === "Reserve") name = focused ? "calendar" : "calendar-outline";
-          else if (route.name === "Settings") name = focused ? "settings" : "settings-outline";
-          return <Ionicons name={name} size={Number(size) || 22} color={color} />;
-        };
-
-        return {
-          headerShown: false,
-          tabBarShowLabel: true,
-          tabBarLabelStyle: { fontWeight: "700" },
-          tabBarActiveTintColor: c.S2,          // ส้มหลัก
-          tabBarInactiveTintColor: c.black,     // ไอคอน/ตัวหนังสือจางลงเอง
-          tabBarStyle: {
-            backgroundColor: c.fullwhite,       // พื้นหลังขาว
-            borderTopColor: c.S3,               // เส้นขอบส้มอ่อน
-            height: 60,
-          },
-          tabBarIcon: ({ focused, size, color }) => icon(focused, size, color),
-        };
+      tabBar={(props)=><TabStyle {...props}/>}
+      screenOptions={{
+        headerShown:false  
       }}
     >
       <Tab.Screen
@@ -48,20 +156,22 @@ export default function V_ButtonNav({ shopId, initialRouteName = "Home" }) {
         component={HomeShopScreen}
         initialParams={{ shopId: String(shopId || "") }}
         options={{ title: "หน้าร้าน" }}
+        
       />
 
       <Tab.Screen
         name="Orders"
         component={OrderShopScreen}
         initialParams={{ shopId: String(shopId || "") }}
-        options={{ title: "ออเดอร์ร้าน" }}
+        options={{ title: "ออเดอร์" }}
       />
 
       <Tab.Screen
         name="Menu"
         component={MenuShopScreen}
         initialParams={{ shopId: String(shopId || "") }}
-        options={{ title: "เมนูในร้าน" }}
+        options={{ title: "เมนู " }}
+        
       />
 
       <Tab.Screen
