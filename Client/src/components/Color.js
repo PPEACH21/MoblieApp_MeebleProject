71af9d93d5c8b1a54a2ca16156745667b1507344
PPEACH21 <sohizz1201@gmail.com>
2025-11-13 00:48:50 +0700

    Feature : UploadProfile

--- Client/src/components/Color.js
+++ Client/src/components/Color.js
@@ -8,6 +8,7 @@ export const BaseColor ={
     black:"#2f2f2f",
     fullblack:"#000",
     white:"#f2f2f2",
+    whitegary:"#bababaff",
     fullwhite:"#fff",
     green:"#2b7424",
     blue:"#2a2aa8ff",
--- Client/src/redux/actions/profileAction.js
+++ Client/src/redux/actions/profileAction.js
@@ -1,4 +1,4 @@
-import { profileLoading,getProfileFailed,getProfileSuccess } from "../slices/profileSlice"
+import { profileLoading,updateProfile,getProfileFailed,getProfileSuccess } from "../slices/profileSlice"
 import { api } from "../../api/axios"
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -29,5 +29,26 @@ export const getProfile=()=>async(dispatch)=>{
         console.log("Error fetching profile:", error);
         dispatch(getProfileFailed(error));
         return error
+    }
+}
+
+
+export const UpdateProfile=({data})=>async(dispatch,getState)=>{
+    dispatch(profileLoading())
+    try{
+        const Auth = getState().auth
+        const profileData = {
+            firstname: data.firstname || "",
+            lastname: data.lastname || "",
+            avatar: data.avatar || "",
+        };
+        const res = await api.put(`/profile/${Auth.user}`, profileData)
+        dispatch(updateProfile(profileData));
+        return profileData;
+    }catch(err){
+        console.log("Error Update profile:", err);
+        const message = err.response?.data?.message || err.message || "Unknown error";
+        dispatch(getProfileFailed(message));
+        return message
     }
 }
